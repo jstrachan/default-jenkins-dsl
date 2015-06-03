276f987f1dc2101a7e4f53a9c5284c93800d98a0
Christian Posta <christian.posta@gmail.com>
2015-06-02 22:49:48 -0700

    hack jenkins build pipeline based on demo we did with Fuse on OSE v2. Still very preliminar, need to fill out the details of deploying to OSE v3/Kube for example

--- seedBuilds.groovy
+++ seedBuilds.groovy
@@ -5,24 +5,52 @@ def excludedProjectNames = []
 def includedProjectNames = []
 
 mavenJob('base-maven-build') {
-  wrappers {
-    timestamps()
-    colorizeOutput()
-    maskPasswords()
-    timeout {
-      elastic(
-              450, // Build will timeout when it take 3 time longer than the reference build duration, default = 150
-              5,   // Number of builds to consider for average calculation
-              120   // 30 minutes default timeout (no successful builds available as reference)
-      )
-      failBuild()
+    keepDependencies(false)
+
+    logRotator(
+            1, // days to keep
+            5, // num to keep
+            -1, // artifact days to keep
+            -1 // artifact num to keep
+    )
+
+    wrappers {
+        timestamps()
+        colorizeOutput()
+        maskPasswords()
+        timeout {
+            elastic(
+                    450, // Build will timeout when it take 3 time longer than the reference build duration, default = 150
+                    5,   // Number of builds to consider for average calculation
+                    120   // 30 minutes default timeout (no successful builds available as reference)
+            )
+            failBuild()
+        }
     }
-  }
-  mavenInstallation('3.3.1')
-  localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
+    mavenInstallation('3.3.1')
+    localRepository(LocalRepositoryLocation.LOCAL_TO_WORKSPACE)
 }
 
-def client = new GitRepoClient('http://gogs.vagrant.local/', 'ceposta', 'RedHat$1')
+freeStyleJob('base-freestyle-build') {
+    wrappers {
+        timestamps()
+        colorizeOutput()
+        maskPasswords()
+        golang('1.4.2')
+        timeout {
+            elastic(
+                    150, // Build will timeout when it take 3 time longer than the reference build duration, default = 150
+                    3,   // Number of builds to consider for average calculation
+                    30   // 30 minutes default timeout (no successful builds available as reference)
+            )
+            failBuild()
+        }
+    }
+}
+
+def username = 'ceposta'
+def password = 'RedHat$1'
+def client = new GitRepoClient('http://gogs.vagrant.local/', username, password)
 repos = client.listRepositories()
 repos.each { repo ->
     def fullName = repo.getFullName()
@@ -33,19 +61,186 @@ repos.each { repo ->
 
     if (!excludedProjectNames.contains(repoName) && (includedProjectNames.contains(repoName) || includedProjectNames.isEmpty())) {
         println "Adding repo ${repoName} to jenkins build"
+        createJobs(repoName, fullName, gitUrl, username, password)
 
-        mavenJob(repoName) {
-            using('base-maven-build')
-            scm {
-                git(gitUrl) {
-                    branch('master')
-                    clean(true)
-                    createTag(false)
-                    cloneTimeout(30)
+    }
+}
+
+def createJobs(repoName, fullName, gitUrl, username, password) {
+    /**
+     * CI Build
+     */
+    mavenJob(repoName + "-ci") {
+        using('base-maven-build')
+        description('Run the build and the unit tests with a specific version. If they pass, move it to the next step.')
+        blockOnDownstreamProjects()
+
+        parameters {
+            stringParam(
+                    'MAJOR_VERSION_NUMBER', // param name
+                    '1.0', // default value
+                    'The major version. We will not use SNAPSHOTs for CD, so need to give a real version'
+            )
+        }
+        scm {
+            git(gitUrl, '*/master') {
+                clean(true)
+                createTag(false)
+                cloneTimeout(30)
+            }
+        }
+        publishers {
+            downstreamParameterized {
+                trigger(repoName + "-it", 'SUCCESS', false){
+                    predefinedProp('TAG_PREFIX', repoName)
+                    predefinedProp('RELEASE_NUMBER', '$MAJOR_VERSION_NUMBER.$BUILD_NUMBER')
                 }
             }
+        }
+        preBuildSteps {
+            shell('git checkout -b ' + repoName + '-$MAJOR_VERSION_NUMBER.$BUILD_NUMBER')
+            maven('versions:set -DnewVersion=$MAJOR_VERSION_NUMBER.$BUILD_NUMBER')
+        }
+        postBuildSteps {
+            conditionalSteps {
+                condition {
+                    status("SUCCESS", "SUCCESS")
+                }
+                // todo this is hardcoded (the location of the git repo; this should use a kube service host/port
+                // todo or do we just use the gitUrl we got back from the /user/repos call and hack in the un/pw?
+                // or use ssh keys?
+                shell('git commit -a -m \'new release candidate\' \n ' +
+                      'git push http://'+username+':'+password+'@gogs.vagrant.local/' + fullName + '.git ' + repoName + '-$MAJOR_VERSION_NUMBER.$BUILD_NUMBER')
+            }
+            conditionalSteps {
+                condition {
+                    status("FAILURE", "FAILURE")
+                }
+                shell('git branch -D '+repoName+'-$MAJOR_VERSION_NUMBER.$BUILD_NUMBER')
+            }
 
-            goals('clean install')
+        }
+
+        goals('clean install')
+    }
+
+    /**
+     * Integration Test Build
+     */
+    mavenJob(repoName + "-it") {
+        using('base-maven-build')
+        description('Run the itests for this module (expected to have a maven profile named itests. If they pass, move it to the next step.')
+        parameters {
+            stringParam(
+                    'TAG_PREFIX', // prefix of the tag we created in the CI step, usually the repo name
+                    repoName, // default value
+                    'The tag prefix we created for the build if it passed the CI step'
+            )
+            stringParam(
+                    'RELEASE_NUMBER', // the release we pushed to nexus and git repo
+                    '', // no good default for this at the moment
+                    'The release we pushed to nexus and git repo'
+            )
+        }
+        scm {
+            git(gitUrl, '${TAG-PREFIX}-${RELEASE_NUMBER}') {
+                clean(true)
+                createTag(false)
+                cloneTimeout(30)
+            }
+        }
+        publishers {
+            downstreamParameterized {
+                trigger(repoName + "-dev-deploy", 'SUCCESS', false){
+                    predefinedProp('TAG_PREFIX', '$TAG_PREFIX')
+                    predefinedProp('RELEASE_NUMBER', '$RELEASE_NUMBER')
+                }
+            }
+        }
+        goals('verify -Pitests')
+    }
+
+    /**
+     * Deploy to DEV
+     */
+    freeStyleJob(repoName + "-dev-deploy") {
+        using('base-freestyle-build')
+        parameters {
+            stringParam(
+                    'TAG_PREFIX', // prefix of the tag we created in the CI step, usually the repo name
+                    repoName, // default value
+                    'The tag prefix we created for the build if it passed the CI step'
+            )
+            stringParam(
+                    'RELEASE_NUMBER', // the release we pushed to nexus and git repo
+                    '', // no good default for this at the moment
+                    'The release we pushed to nexus and git repo'
+            )
+        }
+        scm {
+            git(gitUrl, '${TAG-PREFIX}-${RELEASE_NUMBER}') {
+                clean(true)
+                createTag(false)
+                cloneTimeout(30)
+            }
+        }
+        publishers {
+            downstreamParameterized {
+                trigger(repoName + "-dev-accept", 'SUCCESS', false){
+                    predefinedProp('TAG_PREFIX', '$TAG_PREFIX')
+                    predefinedProp('RELEASE_NUMBER', '$RELEASE_NUMBER')
+                }
+            }
+        }
+    }
+
+    /**
+     * Run Acceptance tests against DEV
+     */
+    mavenJob(repoName + "-dev-accept") {
+        using('base-maven-build')
+        parameters {
+            stringParam(
+                    'TAG_PREFIX', // prefix of the tag we created in the CI step, usually the repo name
+                    repoName, // default value
+                    'The tag prefix we created for the build if it passed the CI step'
+            )
+            stringParam(
+                    'RELEASE_NUMBER', // the release we pushed to nexus and git repo
+                    '', // no good default for this at the moment
+                    'The release we pushed to nexus and git repo'
+            )
+        }
+        scm {
+            git(gitUrl) {
+                branch('master')
+                clean(true)
+                createTag(false)
+                cloneTimeout(30)
+            }
+        }
+
+        goals('clean install')
+    }
+
+    buildPipelineView(repoName + "-cd-pipeline") {
+        selectedJob(repoName + "-ci")
+        title('Continuous Delivery pipeline for ' + repoName)
+        refreshFrequency(30)
+        showPipelineDefinitionHeader(true)
+        showPipelineParameters(true)
+        showPipelineParametersInHeaders(true)
+        displayedBuilds(10)
+        consoleOutputLinkStyle(OutputStyle.NewWindow)
+    }
+
+    buildMonitorView(repoName + "-cd-monitor") {
+        description('All jobs for the ' + repoName + ' CD pipeline')
+        jobs {
+            name(repoName + "-ci")
+            name(repoName + "-it")
+            name(repoName + "-dev-deploy")
+            name(repoName + "-dev-accept")
         }
     }
 }
