85c0244c0f0d645f2290147b94293b4f2d6f7b54
James Strachan <james.strachan@gmail.com>
2015-06-03 12:27:55 +0100

    Update seedBuilds.groovy

--- seedBuilds.groovy
+++ seedBuilds.groovy
@@ -52,8 +52,10 @@ freeStyleJob('base-freestyle-build') {
 def clazz = org.springframework.util.ClassUtils.class
 def cloader = clazz.getClassLoader()
 println "got class ${clazz} from class loader ${cloader}"
-def jarFiles = cloader.jarFiles
-println "got jarFiles ${jarFiles}"
+def urls = cloader.getURLs()
+urls.each { url -> 
+   println "got URL: ${url}"
+}
 
 
 
